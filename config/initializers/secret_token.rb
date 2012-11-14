14847318bae799372c5e350e666eadfacae9aa6a
sword721 <sword721@163.com>
2012-11-14 21:53:08 +0800

    新建项目
    新建项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firstshop::Application.config.secret_token = '746be1ac66ab8a6b75a15ef24d872d2b6aa81dd672f96631f445d0e3b6f98606fba3ef709e8aebc2af1787364cba90df50a0595fee3c2875a9128221c8f0c948'
